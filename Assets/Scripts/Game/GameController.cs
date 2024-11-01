1d4f4274ea603930410a677f29e786b6a06a87b5
PataBasket <basket2323yuta@gmail.com>
2024-11-02 05:45:05 +0900

    feat: :rocker: 戦闘中のBGM追加と音量調整

--- Assets/Scripts/Game/GameController.cs
+++ Assets/Scripts/Game/GameController.cs
@@ -55,6 +55,10 @@ public class GameController : MonoBehaviour
         // AudioManager を参照する
         audioManager = GameObject.FindWithTag("AudioManager").GetComponent<AudioManager>();
         
+        //　ロビーのBGMを止めてゲームのBGMに切り替える
+        audioManager.StopBGM();
+        audioManager.PlayBGM(2);
+        
         // プレイヤリストの取得
         int max = RoomPlayer.Players.Count;
         Debug.Log("max = " + max);
