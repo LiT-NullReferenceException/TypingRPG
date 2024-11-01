e6c8fedfbfedf29dac7a7ef5c9ca99a3e7780b2f
ShibataniMuku <109905506+ShibataniMuku@users.noreply.github.com>
2024-11-02 02:42:01 +0900

    feat: リザルト画面を作成

---
+++ Assets/Scenes/Battle/ResultView.cs
@@ -0,0 +1,28 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+public class ResultView : MonoBehaviour
+{
+    [SerializeField] private CanvasGroup _resultGroup;
+    [SerializeField] private CanvasGroup _gameObject;
+    [SerializeField] private CanvasGroup _selectDialog;
+
+    [ContextMenu("Play")]
+    public async void DiaplayResultView()
+    {
+        RectTransform rect = _gameObject.gameObject.GetComponent<RectTransform>();
+        rect.localScale = Vector3.one * 10;
+        _gameObject.alpha = 0;
+        
+        _resultGroup.gameObject.SetActive(true);
+        _resultGroup.alpha = 0;
+        await _resultGroup.DOFade(1, 1);
+
+        _gameObject.DOFade(1, 0.1f);
+        rect.DOScale(1, 0.1f);
+        await rect.DOPunchPosition(Vector3.one * 10, 1, 30);
+
+        _selectDialog.DOFade(1, 1);
+    }
+}
