04b4e355c3dbe2a3299f059fefe630e4ee83979e
ShibataniMuku <109905506+ShibataniMuku@users.noreply.github.com>
2024-11-02 04:46:39 +0900

    fix: ボタンのホバー時に「※」が表示されるよう修正

---
+++ Assets/Scripts/TextButton.cs
@@ -0,0 +1,28 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TextButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private TextMeshProUGUI _text;
+
+    private string _content;
+
+    private void Awake()
+    {
+        _content = _text.text;
+    }
+
+    public void OnPointerEnter(PointerEventData pointerEventData)
+    {
+        _text.fontStyle = FontStyles.Underline;
+        _text.text = $"※{_text.text}※";
+    }
+    
+    public void OnPointerExit(PointerEventData pointerEventData)
+    {
+        _text.fontStyle = FontStyles.Normal;
+        _text.text = _content;
+    }
+}
