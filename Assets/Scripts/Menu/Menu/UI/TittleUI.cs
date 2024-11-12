351487b6915fa3fc23d16a340e80d0fa9046dd7d
myu-chika-3 <furofuro1626@gmail.com>
2024-11-12 21:06:39 +0900

    feat: ロビーにおけるUIの遷移を管理するスクリプトの大幅な変更

--- Assets/Scripts/Menu/Menu/UI/TittleUI.cs
+++ Assets/Scripts/Menu/Menu/UI/TittleUI.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class TittleUI : MonoBehaviour
 {
+    [SerializeField] private AudioManager audioManager;
     void Awake()
     {
         UIScreen.Focus(GetComponent<UIScreen>());
+    }
+
+    void Start()
+    {
+        // タイトル画面のBGMを再生
+        audioManager.PlayBGM(0);
     }
 }
